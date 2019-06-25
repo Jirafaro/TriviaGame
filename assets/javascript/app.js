085581e788ed762bd7aac864f4e91c5747f8a4c3
Austin Robbins <jirafaro@gmail.com>
2019-06-24 22:21:20 -0700

    added some psuedo code to Js

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// set variables
+// create array of questions
+// set questions to array index
+// create random question on page load 
+// create random question at win / loss 
+// generate four set choices A B C D per question
+// set timer of 20 seconds per question
+// if player chooses correct show you win screen !  
+// if player runs out of time or chooses in correct display correct answer 
+// on final screen show number of correct answers and incorrect answers 
